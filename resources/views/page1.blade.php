183ff3f693f8683abcef8f3586ee869325ecdb20
d.venslav <davyd.venslav@wowweb.agency>
2022-11-26 15:21:18 +0200

    First

---
+++ resources/views/page1.blade.php
@@ -0,0 +1,11 @@
+@extends('index')
+
+@section('content')
+<div>
+    txt
+    <form>
+        <input type="text" value="Enter text...">
+        <button type="submit">Button</button>
+    </form>
+</div>
+@endsection
