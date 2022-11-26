40c6a72150c5d6324dcd1c29ed8e62a0a2bf8c0f
NiLiLiF <davidvenslav@gmail.com>
2022-11-26 15:59:43 +0200

    New commit

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index(){
-        return '<div> My TEXT </div>';
+    public function index($message){
+        return '<div> My TEXT: '.$message.' </div>';
     }
 
     public function about(){
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/','App\Http\Controllers\MainController@index');
-
 Route::get('/about','App\Http\Controllers\MainController@about');
+
+Route::get('/{message}','App\Http\Controllers\MainController@index');
+
+
 
